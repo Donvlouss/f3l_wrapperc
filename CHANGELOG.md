43d6a9efd874df09caaa3a599263c2b61e0c6990
Donvlouss <donvlous0rixk@gmail.com>
2024-11-05 09:26:05 +0800

    impl index and index mut

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # F3l_WrapperC
 
+## [v0.1.0.1] 2024.11.05
+### [Add]
+* Impl `Index` and `IndexMut` for `WPoint`
+### [Change]
+### [Fix]
+
 ## [v0.1.0] 2024.11.04
 ### [Add]
 * `WPoint` 2, 3, 4 with align 4 8 16
--- src/points/imp.rs
+++ src/points/imp.rs
@@ -1,3 +1,5 @@
+use std::ops::{Index, IndexMut};
+
 use f3l_glam::glam::{Vec2, Vec3, Vec4};
 
 use super::*;
@@ -44,6 +46,84 @@ macro_rules! imp_convert_4 {
         }
     };
 }
+macro_rules! index_2 {
+    ($wp: ty) => {
+        impl Index<usize> for $wp {
+            type Output = f32;
+        
+            fn index(&self, index: usize) -> &Self::Output {
+                match index {
+                    0 => &self.x,
+                    1 => &self.y,
+                    _ => panic!("index out of bounds"),
+                }
+            }
+        }
+        impl IndexMut<usize> for $wp {
+            fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+                match index {
+                    0 => &mut self.x,
+                    1 => &mut self.y,
+                    _ => panic!("index out of bounds"),
+                }
+            }
+        }
+    };
+}
+macro_rules! index_3 {
+    ($wp: ty) => {
+        impl Index<usize> for $wp {
+            type Output = f32;
+        
+            fn index(&self, index: usize) -> &Self::Output {
+                match index {
+                    0 => &self.x,
+                    1 => &self.y,
+                    2 => &self.z,
+                    _ => panic!("index out of bounds"),
+                }
+            }
+        }
+        impl IndexMut<usize> for $wp {
+            fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+                match index {
+                    0 => &mut self.x,
+                    1 => &mut self.y,
+                    2 => &mut self.z,
+                    _ => panic!("index out of bounds"),
+                }
+            }
+        }
+    };
+}
+macro_rules! index_4 {
+    ($wp: ty) => {
+        impl Index<usize> for $wp {
+            type Output = f32;
+        
+            fn index(&self, index: usize) -> &Self::Output {
+                match index {
+                    0 => &self.x,
+                    1 => &self.y,
+                    2 => &self.z,
+                    3 => &self.w,
+                    _ => panic!("index out of bounds"),
+                }
+            }
+        }
+        impl IndexMut<usize> for $wp {
+            fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+                match index {
+                    0 => &mut self.x,
+                    1 => &mut self.y,
+                    2 => &mut self.z,
+                    3 => &mut self.w,
+                    _ => panic!("index out of bounds"),
+                }
+            }
+        }
+    };
+}
 
 imp_convert_2!(WPoint2, Vec2);
 imp_convert_2!(WPoint2_8, Vec2);
@@ -54,4 +134,12 @@ imp_convert_3!(WPoint3_16, Vec3);
 imp_convert_4!(WPoint4, Vec4);
 imp_convert_4!(WPoint4_8, Vec4);
 imp_convert_4!(WPoint4_16, Vec4);
-
+index_2!(WPoint2);
+index_2!(WPoint2_8);
+index_2!(WPoint2_16);
+index_3!(WPoint3);
+index_3!(WPoint3_8);
+index_3!(WPoint3_16);
+index_4!(WPoint4);
+index_4!(WPoint4_8);
+index_4!(WPoint4_16);
